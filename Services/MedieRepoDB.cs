349099a3d0760ed62266914605669efadc44f555
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-05-08 13:54:22 +0200

    små ændringer

---
+++ Services/MedieRepoDB.cs
@@ -0,0 +1,15 @@
+﻿using ProtoVinylEksamenGruppe6.Model;
+
+namespace ProtoVinylEksamenGruppe6.Services
+{
+    public class MedieRepoDB
+    {
+        private List<Medie> _medieListe;
+
+        public MedieRepoDB()
+        {
+            MedieRepoDB _medieliste = new MedieRepoDB();
+        }
+
+    }
+}
