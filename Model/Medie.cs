cc350e12cfc7e4bca141aa6ec140ba79836190c5
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-05-08 13:06:14 +0200

    Ændret til dansk i medie

--- Model/Medie.cs
+++ Model/Medie.cs
@@ -6,17 +6,17 @@ namespace ProtoVinylEksamenGruppe6.Model
     public class Medie
     {
         //Properties
-        public string Title { get; set; }
-        public string Artist { get; set; }
-        public int Release { get; set; }
+        public string Titel { get; set; }
+        public string Kunstner { get; set; }
+        public int År { get; set; }
         public string Genre { get; set; }
-        public string Condition { get; set; }
-        public string Price { get; set; }
+        public string Stand { get; set; }
+        public string Pris { get; set; }
         public string Type { get; set; }
         public string VinylType { get; set; }
+        public bool Reserveret {  get; set; }
 
-
-        //Konstruktør
+        //Default Konstruktør
         public Medie() { }
     }
 }
