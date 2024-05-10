a7573d8a8c4b232f5fadf05ab85e32257371ee7c
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-05-10 02:04:51 +0200

    Oprettet reservation med instansfelter og properties samt en default konstruktør. Opdateret Medie til at bruge instansfelter og properties

--- Model/Medie.cs
+++ Model/Medie.cs
@@ -5,18 +5,29 @@ namespace ProtoVinylEksamenGruppe6.Model
 {
     public class Medie
     {
-        //Properties
-        public string Titel { get; set; }
-        public string Kunstner { get; set; }
-        public int År { get; set; }
-        public string Genre { get; set; }
-        public string Stand { get; set; }
-        public string Pris { get; set; }
-        public string Type { get; set; }
-        public string VinylType { get; set; }
-        public bool Reserveret {  get; set; }
+        //instansfelter
+        private string _titel;
+        private string _kunstner;
+        private int _år;
+        private string _genre;
+        private string _stand;
+        private string _pris;
+        private string _type;
+        private string _vinylType;
+        private bool _reserveret;
 
-        //Default Konstruktør
+        //properties
+        public string Titel { get { return _titel; } set { _titel = value; } }
+        public string Kunstner { get { return _kunstner; } set { _kunstner = value; } }
+        public int År { get { return _år; } set { _år = value; } }
+        public string Genre { get { return _genre; } set { _genre = value; } }
+        public string Stand { get { return _stand; } set { _stand = value; } }
+        public string Pris { get { return _pris; } set { _pris = value; } }
+        public string Type { get { return _type; } set { _type = value; } }
+        public string VinylType { get { return _vinylType; } set { _vinylType = value; } }
+        public bool Reserveret { get { return _reserveret; } set { _reserveret = value; } }
+
+        //konstruktør
         public Medie() { }
     }
 }
--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -2,5 +2,17 @@
 {
     public class Reservation
     {
+        //instansfelter
+        private Medie _medie;
+        private string _kundeNavn;
+        private string _kundeTelefon;
+
+        //properties
+        public Medie Medie { get { return _medie; } set { _medie = value; } }
+        public string KundeNavn { get {  return _kundeNavn; } set { _kundeNavn = value; } }
+        public string KundeTelefon { get { return _kundeTelefon; } set { _kundeTelefon = value; }}
+
+        //konstruktør
+        public Reservation() { }
     }
 }
