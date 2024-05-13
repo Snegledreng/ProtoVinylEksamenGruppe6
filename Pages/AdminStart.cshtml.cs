c84d595f0d66ba5f6eacc255fae328a706b838a6
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-05-13 09:27:12 +0200

    admin statr oprettet

---
+++ Pages/AdminStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProtoVinylEksamenGruppe6.Pages
+{
+    public class AdminStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
