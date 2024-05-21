19512610486bb606eaf220d859bebb5327e11667
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-05-21 09:41:48 +0200

    AdminDelete

---
+++ Pages/Admin/AdminDelete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProtoVinylEksamenGruppe6.Pages.Admin
+{
+    public class AdminDeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
