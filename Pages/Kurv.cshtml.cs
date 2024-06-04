04f8fe9b78b1eb64e7287fdb321c440ed3381164
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-06-04 10:48:22 +0200

    Validation for reservation på kurv.cshtms Validation på kurv pagemodel Validation på reservationsklassen

--- Pages/Kurv.cshtml.cs
+++ Pages/Kurv.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProtoVinylEksamenGruppe6.Model;
 using ProtoVinylEksamenGruppe6.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProtoVinylEksamenGruppe6.Pages
 {
@@ -17,10 +18,21 @@ namespace ProtoVinylEksamenGruppe6.Pages
         public List<Medie> Medier { get; set; } = new List<Medie> { };
         public int TotalPris {  get; set; }
 
-        [BindProperty] public string KundeNavn { get; set; }
-        [BindProperty] public string KundeTelefon { get; set; }
+        [BindProperty]
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "Navnet skal være mindst 2 tegn")]
+        public string KundeNavn { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Du skal indtaste et telefonnummer")]
+        [StringLength(12, MinimumLength = 8, ErrorMessage = "Telefonnummer skal være mellem 8-12 tegn")]
+        public string KundeTelefon { get; set; }
 
         public void OnGet()
+        {
+            LoadSessionData();//Vi kalder metoden LoadSessionData
+        }
+
+        private void LoadSessionData()
         {
             Kurv kurv = null;
             try
@@ -54,6 +66,12 @@ namespace ProtoVinylEksamenGruppe6.Pages
 
         public IActionResult OnPostOpretReservation()
         {
+            if (!ModelState.IsValid)
+            {
+                LoadSessionData();
+                return Page();
+            }
+
             Kurv kurv = SessionHelper.Get<Kurv>(HttpContext);
             List<Reservation> reservationList = new List<Reservation>();
             foreach (var m in kurv.MedieList)
